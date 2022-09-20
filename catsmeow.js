cf99dd090d4fbed15a613f5b7fa338392ba33250
Will Wadman <wadmanwill@gmail.com>
2022-09-20 14:41:23 +0000

    this was the cats meow activity.

---
+++ catsmeow.js
@@ -0,0 +1,10 @@
+var catSays = function(max) {
+  var catMessage = "";
+  for (var i = 0; i < max; i++) {
+    catMessage += "meow ";
+  }
+  return catMessage;
+  {console.log(catSays())}
+};
+
+catSays();
