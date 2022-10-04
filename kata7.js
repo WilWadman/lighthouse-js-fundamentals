d587943adb9e9fb52b88614a39a4f754c866a70e
Will Wadman <wadmanwill@gmail.com>
2022-10-04 13:38:19 +0000

    this is the kata7 activity

---
+++ kata7.js
@@ -0,0 +1,31 @@
+const checkAir = function (samples, threshold) {
+
+  var dirtyCount = 0;
+  for (let i = 0; i < samples.length; i++){
+  if (samples[i] === 'dirty')
+  dirtyCount++;
+  }
+  
+  if (threshold < dirtyCount/10) {
+  return 'Polluted';
+  }
+  else if (threshold > dirtyCount /10){
+  return 'Clean';
+  };
+  
+
+}
+console.log(checkAir(
+['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
